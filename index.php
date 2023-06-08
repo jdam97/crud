a567ba78b2b2d14a1683ce7a80e9875226d5b59d
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-08 17:13:50 -0500

    feat: :sparkles: tabla
    Agrego la estructura de las tablas

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crud con php</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -48,8 +49,40 @@
             
         </div>
     </form>
+    <div class="contenedor-tabla">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">Direccion</th>
+                <th scope="col">Edad</th>
+                <th scope="col">Email</th>
+                <th scope="col">Horario de entrada</th>
+                <th scope="col">Team</th>
+                <th scope="col">Trainer</th>
+                <th scope="col">Editar</th>
+            </tr>
+        </thead>
 
-   
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Jonathan</td>
+                <td>Alvarez</td>
+                <td>calle falsa 123</td>
+                <td>25</td>
+                <td>@mdo</td>
+                <td>5:00am</td>
+                <td>M3</td>
+                <td>Miguel</td>
+                <td><button type="submit"><i class="fa-solid fa-eye"></i></td></button> 
+            </tr>
+        </tbody>
+    </table>
+    </div>
+
 
 </div>
 
